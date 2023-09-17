0e7bba7b598e40e30e63f29003fcaa4aa580469e
FENG Zhichao <flankerfc@gmail.com>
2023-09-17 22:29:34 +0800

    A dropdown input to switch between collections.

--- src/app/collections/[name]/layout.tsx
+++ src/app/collections/[name]/layout.tsx
@@ -1,14 +1,39 @@
-import {AppShell, AppShellMain, AppShellHeader, Group} from "@mantine/core";
+'use client'
 
-export default function Layout({children}) {
+import {AppShell, AppShellMain, AppShellHeader, Group, Select} from "@mantine/core";
+import {useGetCollections, useGetConfig} from "@/lib/client/query";
+import {useRouter} from "next/navigation";
+
+export default function Layout({children, params}) {
+  const router = useRouter()
+  const {data: config} = useGetConfig()
+  const {name: currentCollectionName} = params
+  const {data: collections} = useGetCollections(config)
+
+  const collectionChanged = (name) => {
+    router.push(`/collections/${name}`)
+  }
+
   return (
     <AppShell
       header={{height: 60}}
       padding="md"
     >
       <AppShellHeader>
-        <Group h="100%" px="md">
-          Vector UI
+        <Group h="100%" px="md" justify="space-between">
+          <Group>
+            Vector UI
+            {collections ? (
+              <Select
+                allowDeselect={false}
+                value={currentCollectionName}
+                data={collections?.data.map((c) => c.name)}
+                onChange={collectionChanged}
+              />
+            ) : (
+              <></>
+            )}
+          </Group>
         </Group>
       </AppShellHeader>
       <AppShellMain>
