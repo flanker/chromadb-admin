7adaa0a50ed5f2665dc5771047e10c4baba1373b
FENG Zhichao <flankerfc@gmail.com>
2023-09-23 17:40:16 +0800

    lint-ed

--- script/generate_mock_data.js
+++ script/generate_mock_data.js
@@ -1,16 +1,17 @@
-import { ChromaClient } from "chromadb";
-const chroma = new ChromaClient({ path: "http://localhost:8000" });
-const collection = await chroma.createCollection({ name: "collection-name" });
+import { ChromaClient } from 'chromadb'
+
+const chroma = new ChromaClient({ path: 'http://localhost:8000' })
+const collection = await chroma.createCollection({ name: 'collection-name' })
 for (let i = 0; i < 2000; i++) {
   await collection.add({
-    ids: ["test-id-" + i.toString()],
+    ids: ['test-id-' + i.toString()],
     embeddings: [1, 2, 3, 4, 5],
-    documents: ["test"],
-  });
+    documents: ['test'],
+  })
 }
 const queryData = await collection.query({
   queryEmbeddings: [1, 2, 3, 4, 5],
-  queryTexts: ["test"],
-});
+  queryTexts: ['test'],
+})
 
 console.log(queryData)
