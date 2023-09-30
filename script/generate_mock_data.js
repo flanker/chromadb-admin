2f9ebdb997b78cec0e24f07795076cfdbcbe51be
FENG Zhichao <flankerfc@gmail.com>
2023-09-30 12:35:17 +0800

    script to generate some mock embeddings.

--- script/generate_mock_data.js
+++ script/generate_mock_data.js
@@ -1,14 +1,20 @@
 import { ChromaClient } from 'chromadb'
 
-const chroma = new ChromaClient({ path: 'http://localhost:8000' })
-const collection = await chroma.createCollection({ name: 'collection-name' })
+const chromaPath = 'http://localhost:8002'
+
+const chroma = new ChromaClient({ path: chromaPath })
+const collection = await chroma.createCollection({ name: 'test-collection' })
 for (let i = 0; i < 2000; i++) {
   await collection.add({
     ids: ['test-id-' + i.toString()],
     embeddings: [1, 2, 3, 4, 5],
     documents: ['test'],
   })
+  process.stdout.write('.')
 }
+console.log('')
+console.log('Done')
+
 const queryData = await collection.query({
   queryEmbeddings: [1, 2, 3, 4, 5],
   queryTexts: ['test'],
